53b9f371ef736f595eb91b5ffdef55943eef5b1e
Maciej Lewinski <maciej.lewinski@zaven.co>
2017-12-02 12:14:12 +0100

    Add View category

--- app/src/main/java/com/atc/planner/data/models/local/PlaceCategory.kt
+++ app/src/main/java/com/atc/planner/data/models/local/PlaceCategory.kt
@@ -3,7 +3,7 @@ package com.atc.planner.data.models.local
 import com.atc.planner.data.models.remote.sygic_api.Category
 
 enum class PlaceCategory {
-    SIGHT, FOOD, AMUSEMENT, ART_GALLERY, MUSEUM, PHYSICAL_ACTIVITY, OTHER
+    SIGHT, FOOD, AMUSEMENT, ART_GALLERY, MUSEUM, PHYSICAL_ACTIVITY, VIEW, OTHER
 }
 
 fun Category?.asPlaceCategory(): PlaceCategory = when (this) {
@@ -13,6 +13,10 @@ fun Category?.asPlaceCategory(): PlaceCategory = when (this) {
     Category.GOING_OUT,
     Category.PLAYING,
     Category.RELAXING -> PlaceCategory.AMUSEMENT
+
+    Category.HIKING -> PlaceCategory.VIEW
+
+    Category.DOING_SPORTS -> PlaceCategory.PHYSICAL_ACTIVITY
 
     Category.SIGHTSEEING -> PlaceCategory.SIGHT
 
